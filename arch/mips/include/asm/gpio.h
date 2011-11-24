d8bfbebcf3eb2a476885c45ce40793304222b9ea
Daniel Schwierzeck <daniel.schwierzeck@googlemail.com>
2011-11-24 03:57:54 +0000

    MIPS: add header file for generic GPIO API
    Signed-off-by: Daniel Schwierzeck <daniel.schwierzeck@googlemail.com>
    Acked-by: Marek Vasut <marek.vasut@gmail.com>

---
+++ arch/mips/include/asm/gpio.h
@@ -0,0 +1,13 @@
+/*
+ * This file is released under the terms of GPL v2 and any later version.
+ * See the file COPYING in the root directory of the source tree for details.
+ *
+ * Copyright (C) 2011 Daniel Schwierzeck, daniel.schwierzeck@googlemail.com
+ */
+
+#ifndef __ASM_MIPS_GPIO_H__
+#define __ASM_MIPS_GPIO_H__
+
+#include <asm/arch/gpio.h>
+
+#endif /* __ASM_MIPS_GPIO_H__ */
